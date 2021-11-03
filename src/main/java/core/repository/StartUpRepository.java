6ea363e0acdc6dcbe2326db9d4bd1a2f7f511d3b
Paige Traviata <Paigetraviata96@hotmail.com>
2021-11-03 17:58:41 +0100

    updates to product controller and added methods to repo-interface
    Created general setup for productcontroller.
    Implemented methods from JSONRepository to repo-interface

--- src/main/java/core/repository/StartUpRepository.java
+++ src/main/java/core/repository/StartUpRepository.java
@@ -1,6 +1,34 @@
 package core.repository;
 
-import web.dtos.StoreUserDTO;
+import core.model.*;
+
+import java.util.ArrayList;
 
 public interface StartUpRepository {
+
+    ArrayList<Store> getAllStores();
+
+    ArrayList<StoreUser> getAllEmployees(int storeId);
+
+    ArrayList<Product> getAllProducts(int storeId);
+
+    Store getAStore(int storeId);
+
+    StoreUser getAnEmployee(int storeId, String userName);
+
+    Product getAProduct(int storeId, int productId);
+
+    /*****************************************************/
+
+    void createStoreUser(int storeId, StoreUser newEmployee);
+
+    void updateStoreUser(int storeId, String oldUsername, StoreUser updatedEmployee);
+
+    void deleteStoreUser(int storeId, String userName);
+
+    void createProduct(int storeId, Product newProduct);
+
+    void updateProduct(int storeId, int productId, Product newProduct);
+
+    void deleteProduct(int storeId, int productId);
 }
--- src/main/java/web/controller/ProductController.java
+++ src/main/java/web/controller/ProductController.java
@@ -1,11 +1,37 @@
 package web.controller;
 
 import core.repository.StartUpRepository;
+import io.javalin.http.Context;
 
 public class ProductController {
     public final StartUpRepository startUpRepository;
 
     public ProductController(StartUpRepository startUpRepository) {
         this.startUpRepository = startUpRepository;
+    }
+
+    public void getAllProducts(Context context) {
+        int storeId = Integer.parseInt(context.pathParam("###"));
+
+    }
+
+    public void getAProduct(Context context) {
+        int storeId = Integer.parseInt(context.pathParam("###"));
+        int productId = Integer.parseInt(context.pathParam("###"));
+    }
+
+    public void createProduct(Context context) {
+        int storeId = Integer.parseInt(context.pathParam("###"));
+
+    }
+
+    public void updateProduct(Context context) {
+        int storeId = Integer.parseInt(context.pathParam("###"));
+        int productId = Integer.parseInt(context.pathParam("###"));
+    }
+
+    public void deleteProduct(Context context) {
+        int storeId = Integer.parseInt(context.pathParam("###"));
+        int productId = Integer.parseInt(context.pathParam("###"));
     }
 }
