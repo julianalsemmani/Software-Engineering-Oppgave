60ccda856b6053137230a67171868ec1c3d8b362
Mustafa Sahin <70243532+Mosse-hiof@users.noreply.github.com>
2021-11-16 14:24:25 +0100

    startUp dtos updated

--- src/main/java/web/dtos/startUp/StartUpResponseBody.java
+++ src/main/java/web/dtos/startUp/StartUpResponseBody.java
@@ -1,4 +1,20 @@
 package web.dtos.startUp;
 
+import core.model.StartUp;
+import core.model.User;
+
+import java.util.UUID;
+
 public class StartUpResponseBody {
+    public String startUpName;
+    public String address;
+    public int phoneNumber;
+    public int owner;
+
+    public StartUpResponseBody(StartUp startup){
+        this.startUpName = startup.name;
+        this.address = startup.address;
+        this.phoneNumber = startup.phoneNumber;
+    }
+
 }
