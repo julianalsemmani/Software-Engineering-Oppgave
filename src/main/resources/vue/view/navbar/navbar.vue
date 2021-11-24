2acec3889c9a9783ac4a214d1c58f4813c427674
ealangst <ealangst@hiof.no>
2021-11-24 18:19:09 +0100

    Controller method for getting logged in user "/api/me"

---
+++ src/main/resources/vue/view/navbar/navbar.vue
@@ -0,0 +1,25 @@
+<template id="navbar">
+  <nav class="navbar">
+    <section>
+      <span>{{loggedInUser.username}}</span>
+    </section>
+  </nav>
+</template>
+
+<script>
+Vue.component("navbar", {
+  template: "#navbar",
+  data: ({
+    loggedInUser: {}
+  }),
+  created() {
+
+  }
+})
+</script>
+
+<style scoped>
+.navbar {
+
+}
+</style>
