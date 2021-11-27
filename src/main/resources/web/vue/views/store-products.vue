da70663c44b70d728b34371c71554f35211e54f3
Ibrahim-HIOF <70488743+Ibrahim-HIOF@users.noreply.github.com>
2021-11-27 20:32:52 +0100

    Fixed store-products.vue html and added some css to style.css

--- src/main/resources/web/vue/views/store-products.vue
+++ src/main/resources/web/vue/views/store-products.vue
@@ -5,21 +5,24 @@
     <main id="main">
       <article v-for="product in store.products">
         <section class="article-box" v-if="product.saleMethod?.sale" article>
-          <p>{{product.name}}</p>
-          <p>{{product.saleMethod.sale.price}}</p>
+          <img src="https://cdn.pixabay.com/photo/2020/03/01/17/50/monalisa-4893660_640.jpg" alt="Girl in a jacket" width="200" height="200">
+          <p>Product name: {{product.name}}</p>
+          <p>Product price: {{product.saleMethod.sale.price}}</p>
         </section>
 
         <section class="article-box" v-else-if="product.saleMethod?.auction">
+          <img src="https://imaginary.abcmedia.no/resize?width=980&interlace=true&url=https%3A%2F%2Fimaginary.abcmedia.no%2Fpipe%3Furl%3Dhttps%253A%252F%252Fabcnyheter.drpublish.aptoma.no%252Fout%252Fimages%252Farticle%252F%252F2013%252F11%252F06%252F195151679%252F1%252Foriginal%252F2234089.jpg" alt="Girl in a jacket" width="200" height="200">
           <p>Product name: {{product.name}}</p>
           <p>Minimum bid: {{product.saleMethod.auction.minimumBid}} NOK</p>
           <p>Minimum allowed bid: {{product.saleMethod.auction.minimumBidIncrement}} NOK</p>
-          <p>Auction start time:{{new Date(product.saleMethod.auction.auctionStartTime).toLocaleString()}}</p>
-          <p>Auction end time:{{new Date(product.saleMethod.auction.auctionEndTime).toLocaleString()}}</p>
+          <p>Auction start time: {{new Date(product.saleMethod.auction.auctionStartTime).toLocaleString()}}</p>
+          <p>Auction end time: {{new Date(product.saleMethod.auction.auctionEndTime).toLocaleString()}}</p>
         </section>
 
         <section class="article-box" v-else>
-          <p>{{product.name}}</p>
-          <p>Not for sale</p>
+          <img src="https://media.snl.no/media/7650/standard_Mona_Lisa.jpg" alt="Girl in a jacket" width="200" height="200">
+          <p>Product name: {{product.name}}</p>
+          <p>Product price: Not for sale</p>
         </section>
       </article>
     </main>
