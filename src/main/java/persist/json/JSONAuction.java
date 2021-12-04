406ec7ca6e9bb36d963d1a7cf93f4cfe23580ce4
ealangst <ealangst@hiof.no>
2021-12-04 19:54:17 +0100

    Persist auction bids

--- src/main/java/persist/json/JSONAuction.java
+++ src/main/java/persist/json/JSONAuction.java
@@ -6,16 +6,15 @@ import core.model.Store;
 import core.model.User;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JSONAuction implements JSONDeserializer<Auction> {
     public int minimumBid;
     public int minimumBidIncrement;
     public int buyoutPrice;
     public Instant auctionStartTime, auctionEndTime;
-    public List<AuctionBid> bidHistory;
+    public JSONAuctionBid[] bidHistory;
 
     public JSONAuction() {
         
@@ -27,11 +26,14 @@ public class JSONAuction implements JSONDeserializer<Auction> {
         buyoutPrice = auction.buyoutPrice;
         auctionStartTime = auction.auctionStartTime;
         auctionEndTime = auction.auctionEndTime;
-        bidHistory = auction.bidHistory;
+        bidHistory = auction.bidHistory.stream().map(JSONAuctionBid::new).toArray(JSONAuctionBid[]::new);
     }
 
     @Override
     public Auction deserialize(Map<UUID, User> idUserMap, Map<UUID, Store> idStoreMap) {
-        return new Auction(minimumBid, minimumBidIncrement, buyoutPrice, auctionStartTime, auctionEndTime, bidHistory);
+        return new Auction(minimumBid, minimumBidIncrement, buyoutPrice, auctionStartTime, auctionEndTime,
+                Arrays.stream(bidHistory)
+                        .map(bid -> bid.deserialize(idUserMap, idStoreMap))
+                        .collect(Collectors.toCollection(ArrayList::new)));
     }
 }
