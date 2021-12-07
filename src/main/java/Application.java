34f54b26fe424052c4bca76a8afe2e114dde6846
ealangst <ealangst@hiof.no>
2021-12-07 18:11:00 +0100

    Moved application and cleared database

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,12 @@
-package web;
-
 import core.repository.Repository;
 import core.service.Service;
 import persist.JSONRepository;
+import web.WebServer;
 
 
 public class Application {
     public static void main(String[] args) {
-        Repository repository = new JSONRepository("src/main/resources/persist/test.db.json");
+        Repository repository = new JSONRepository("src/main/resources/persist/prototype.db.json");
         Service service = new Service(repository);
         WebServer webServer = new WebServer(service);
 
